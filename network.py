1a26c59d11789e8be1513e53acd19f2252f1a8b5
Hyeokjune Jeon <fantajeon@cnu.ac.kr>
2018-05-14 19:26:07 +0900

    policy loss 방향 수정

--- network.py
+++ network.py
@@ -78,11 +78,11 @@ class ActorCritic():
                 entropy = -tf.reduce_sum(self.policy * self.log_softmax_policy,axis=1)
                 log_p_s_a = tf.reduce_sum(self.log_softmax_policy * tf.one_hot(self.action,nA, dtype=tf.float32),axis=1)
 
-                self.policy_loss = tf.reduce_mean(tf.stop_gradient(advantage)*log_p_s_a)
                 self.entropy_loss = tf.reduce_mean(entropy)
+                self.policy_loss = tf.reduce_mean(tf.stop_gradient(advantage)*log_p_s_a) + entropy_beta*self.entropy_loss
                 self.value_loss = tf.reduce_mean(tf.square(advantage_hubber))
 
-                loss = self.value_loss * 0.5 + self.policy_loss + entropy_beta*self.entropy_loss
+                loss = self.value_loss * 0.5 + self.policy_loss 
                 self.gradients = tf.gradients(loss,self.train_vars)
                 self.var_norms = tf.global_norm(self.train_vars)
 
